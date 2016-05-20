486751794b4ad69af5a8b430db66c9d1a37e4afa
skwang <skwang@princeton.edu>
2016-05-20 16:27:23 -0400

    update main

--- main.sh
+++ main.sh
@@ -5,6 +5,6 @@ do
     echo "Press [CTRL+C] twice to exit properly.."
     # runs the python script, waits for 10 seconds after completing, and repeats
     # TODO: maybe make it sleep for longer? python script sleeps until midnight
-    python facerec_main.py -t ~/Dropbox/subsetfaces && sleep 1
+    python facerec_main.py -t ~/Dropbox/EEdisplayfaces && sleep 1
     sleep 10
 done
