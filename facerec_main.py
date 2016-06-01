bf939fb9b1faca09705a177b711efa0d8fc43537
Sai <sai@nat-oitwireless-inside-vapornet100-a-17472.Princeton.EDU>
2016-06-01 01:24:16 -0400

    Remove prediction aggregation in fecerec_main.py; Use high-confidence predictions instead

--- facerec_main.py
+++ facerec_main.py
@@ -1,6 +1,6 @@
 '''
 Author: Kevin Wang
-Last updated: 5/19/16
+Last updated: 6/1/16    by Sanket Satpathy
 Used with Python 2.7
 
 Description: 
@@ -22,16 +22,21 @@ from PIL import Image
 from recognizer_util import get_images_and_labels
 from recognizer_util import infer_images_and_labels
 from openface_recognizer import OpenfaceRecognizer
+from pprint import pprint
 
+F_WIDTH = 640
+F_HEIGHT = 480
+display_feed = True
 
 # flag for whether or not to use the VGG recognizer for verification
-useVGG = 0
-if useVGG == 1:
+useVGG = False
+if useVGG:
     from vgg_recognizer import VGGRecognizer
 
 # the minimum proportion of the weight we need to be "confident"
 # about a face and save it to a file
-WEIGHT_CONFIDENCE = 0.6
+WEIGHT_CONFIDENCE = 40
+NEAREST_NEIGHBORS = 2
 
 # for tracking faces
 class TrackFace:
@@ -42,107 +47,36 @@ class TrackFace:
     # how many frames ago this face was updated
     lastUpdated = None
 
-    # what was the last prediction for this face and their weights
-    lastPredictions = None
-    lastWeights = None
-
-    # when the face was last predicted
-    lastPredictionTime = None
-
-    # how often to update face prediction, in seconds
-    updatePeriod = 0.25
-
-    # max distance to consider it to be the same face (in pixels)
+    # max distance (in pixels) to consider it to be the same face
     max_distance_tolerance = 55
 
-    # max number of previous predictions to save and use for majority vote
-    num_majority_vote = 10
+    # fractional error within which we consider it to be the same face
+    area_tolerance = 0.2
 
-    # time of last VGG prediction
-    lastVGGTime = None
-    # how often to update the VGG period
-    VGGperiod = 10
-    # how much extra the VGG weight is 
-    VGGweight = 20
-
-    # time of last JSON save
-    lastJSONsave = None
-    JSONsavePeriod = 5
+    # time within which we consider it to be the same face
+    time_tolerance = 1
 
     # constructor. Pass in location of the center of the face
-    def __init__(self, x, y):
+    def __init__(self, x, y, w, h):
         self.x = x
         self.y = y
-        self.lastUpdated = 0 
-        self.lastPredictions = []
-        self.lastWeights = []
-        self.lastVGGTime = None
-        self.lastJSONsave = None
+        self.w = w
+        self.h = h
+        self.lastUpdated = time.time() 
 
     # check if a detected face should be considered the same face, using only location
-    def checkSimilar(self, x, y):
+    def checkSimilar(self, x, y, w, h):
         distance = math.sqrt((self.x - x)**2 + (self.y - y)**2)
-        if distance > self.max_distance_tolerance:
-            self.lastUpdated += 1
+        area_difference = abs(self.w * self.h - w * h) / float(self.w * self.h)
+        time_difference = time.time() - self.lastUpdated
+        if distance > self.max_distance_tolerance or \
+            area_difference > self.area_tolerance or \
+            time_difference > self.time_tolerance:
+            self.lastUpdated = time.time()#+= 1   # only counts frames!!  these frames may be far apart in time
             return False
         else: 
-            self.lastUpdated = 0 
+            self.lastUpdated = time.time()#0 
             return True
-
-    # update the prediction with the given weight, and return the majority vote 
-    # prediction of the last k predictions
-    def updatePrediction(self, pred, weight=1.):
-        if self.lastPredictionTime is None or time.time() - self.lastPredictionTime > self.updatePeriod:
-            self.lastPredictionTime = time.time()
-            self.lastPredictions.append(pred)
-            self.lastWeights.append(weight)
-            if len(self.lastPredictions) > 20:
-                del self.lastPredictions[0]
-
-        return self.getMajorityPrediction(print_=True)
-
-        
-    # return the majority vote prediction of the last k predictions
-    def getMajorityPrediction(self, print_=False):
-        if len(self.lastPredictions) == 0:
-            return None
-        # calculate weighted totals and return max weight
-        scale = {}
-        for i in range(0, len(self.lastPredictions)):
-            predi = self.lastPredictions[i]
-            weighti = self.lastWeights[i]
-            if predi in scale:
-                scale[predi] += weighti
-            else:
-                scale[predi] = weighti
-
-        totalweight = float(sum(scale.values()))
-        if print_:
-            # print confidence measures, normaled from 0 to 1, sorted from high to low
-            for name, weight in sorted(scale.iteritems(), key=operator.itemgetter(1))[::-1]:
-                print '\t',
-                print name,
-                print "{0:.2f}".format(weight/totalweight)
-        return max(scale.iteritems(), key=operator.itemgetter(1))[0]
-
-    # return the majority vote prediction of the last k predictions and its proportion of weight
-    def getMajorityPredictionAndWeight(self):
-        if len(self.lastPredictions) == 0:
-            return None
-        # calculate weighted totals and return max weight
-        scale = {}
-        for i in range(0, len(self.lastPredictions)):
-            predi = self.lastPredictions[i]
-            weighti = self.lastWeights[i]
-            if predi in scale:
-                scale[predi] += weighti
-            else:
-                scale[predi] = weighti
-
-        totalweight = float(sum(scale.values()))
-        majorityprediction = max(scale.iteritems(), key=operator.itemgetter(1))[0]
-        return majorityprediction, scale[majorityprediction]/totalweight
-
             
 
 # initializes an opencv webcam with specified parameters
@@ -158,21 +92,25 @@ def updateJSON(prediction, predictiontime):
     readfilepath = './eeslides/eeslides/static/updates_django.json'
     writefilepath = './eeslides/eeslides/static/updates_facerec.json'
 
-    if os.path.isfile(readfilepath):
-        with open(readfilepath, 'r') as readfile:
-            filedata = json.load(readfile)
-    else:
-        filedata = {}
+    try:
+        if os.path.isfile(readfilepath):
+            with open(readfilepath, 'r') as readfile:
+                filedata = json.load(readfile)
+        else:
+            filedata = {}
 
-    now = datetime.datetime.now()
-    currdate = now.strftime("%Y-%m-%d")
+        now = datetime.datetime.now()
+        currdate = now.strftime("%Y-%m-%d")
 
-    if currdate not in filedata:
-        filedata[currdate] = []
-    filedata[currdate].append({"prediction": prediction, "time": predictiontime, "parsed":False})
+        if currdate not in filedata:
+            filedata[currdate] = []
+        filedata[currdate].append({"prediction": prediction, "time": predictiontime, "parsed":False})
 
-    with open(writefilepath, 'w+') as outfile:
-        json.dump(filedata,outfile, indent = 4, sort_keys = True)
+        with open(writefilepath, 'w+') as outfile:
+            json.dump(filedata, outfile, indent = 4, sort_keys = True)
+    except:
+        print 'WARNING: Unable to read JSON file'
+        pass
 
 # retrieves training images and trains the passed recognizer on them
 def train_routine(recognizer, trainingdir):
@@ -219,28 +157,31 @@ def sub_routine(timelimit=86400):
     # create a face recognizer 
     print 'Initializing recognizer.'
     now = time.time()
-    if useVGG == 1:
-        vggrecognizer = VGGRecognizer(knn=3)
-    recognizer = OpenfaceRecognizer(knn=3)
+    if useVGG:
+        recognizer = VGGRecognizer(knn=NEAREST_NEIGHBORS)
+    else:
+        recognizer = OpenfaceRecognizer(knn=NEAREST_NEIGHBORS)
     print '\tTime : {0:.2f} s'.format(time.time() - now)
 
     
     # train for the first time
-    if useVGG == 1:
-        ylabels_vgg = vggrecognizer.train(images, labels)
-    recognizer, ylabels = train_routine(recognizer, args['traindir'])
+    if useVGG:
+        recognizer, ylabels_vgg = train_routine(recognizer, args['traindir'])
+    else:
+        recognizer, ylabels = train_routine(recognizer, args['traindir'])
 
     # set up webcam
-    fwidth = 1200
-    fheight = 800
-
-    cap = initializeWebcam(cam=0,width=fwidth,height=fheight)
+    fwidth = F_WIDTH            # default frame dimensions
+    fheight = F_HEIGHT
+    cap = initializeWebcam(cam=1,width=fwidth,height=fheight)
     # for tracking fps
     prevtime = None
     currtime = None
 
     # for tracking faces
     trackedfaces = []
+    lastJSONsave = None
+    JSONsavePeriod = 5
 
     print 'Starting the capture.'
     timelimit_start = time.time()
@@ -248,9 +189,21 @@ def sub_routine(timelimit=86400):
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
-        # create a copy for display and writing on
-        cpframe = np.copy(frame)
-        dlibfaces = dlib_detector(frame)
+        fheight, fwidth = frame.shape[:-1]
+
+        # now = time.time()
+        x_offset = 0
+        y_offset = 0
+        dlibfaces = dlib_detector(frame)    # ~0.5 s
+        if not dlibfaces:   # zoom in if no faces found
+            x_offset = 100
+            x_span = 440
+            y_offset = 250
+            y_span = 120
+            cv2.rectangle(frame,(x_offset, y_offset),(x_offset+x_span, y_offset+y_span),(0,0,255),2)
+            dlibfaces = dlib_detector(frame[y_offset:420, x_offset:-100].astype('uint8'), 2)
+        # print '\tDetecting faces took {0:.2f} seconds'.format(time.time() - now)
+
         faces = []
         for df in dlibfaces:
             # extract the x, y coordinates and width/height, 
@@ -258,8 +211,8 @@ def sub_routine(timelimit=86400):
             centerx = (df.right() - df.left())/2 + df.left()
             centery = (df.bottom() - df.top())/2 + df.top()
             scalefactor = 2.
-            x = max(0, int(centerx - (centerx - df.left())*scalefactor))
-            y = max(0, int(centery - (centery - df.top())*scalefactor))
+            x = x_offset + max(0, int(centerx - (centerx - df.left())*scalefactor))
+            y = y_offset + max(0, int(centery - (centery - df.top())*scalefactor))
             w = min(fwidth - x, int((centerx - df.left())*scalefactor*2))
             h = min(fheight - y, int((centery - df.top())*scalefactor*2))
             faces.append((x, y, w, h))
@@ -268,24 +221,25 @@ def sub_routine(timelimit=86400):
         for (x,y,w,h) in faces:
             # look for a matching face
             matchedface = None
+
             for oldface in trackedfaces:
-                if oldface.checkSimilar(x, y):
+                if oldface.checkSimilar(x, y, w, h):
                     matchedface = oldface
 
-            # remove faces that haven't appeared in the last 5 frames
+            # remove faces that haven't appeared in the last 3 seconds
             for oldface in trackedfaces:
-                if oldface.lastUpdated > 5:
+                if oldface.lastUpdated > 3:
                     trackedfaces.remove(oldface)
 
             # if no matching face found, then create a new one
             if matchedface is None:
-                matchedface = TrackFace(x, y)
+                matchedface = TrackFace(x, y, w, h)
                 trackedfaces.append(matchedface)
                 # display rectangle and text over face, in green
-                cv2.rectangle(cpframe,(x,y),(x+w,y+h),(0,255,0),2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
             else:
                 # display rectangle and text over face, in blue
-                cv2.rectangle(cpframe,(x,y),(x+w,y+h),(255,0,0),2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
             # run the prediction and update the matchedface accordingly
             roi = frame[y:y+h, x:x+w]
@@ -296,94 +250,43 @@ def sub_routine(timelimit=86400):
             # if we have a valid prediction
             if prediction is not None:
                 dist, ind = prediction
-                print dist,
-                print ind,
 
-                # only update prediction if the two closest neighbors agree
-                if ylabels[ind[0,0]] == ylabels[ind[0, 1]]:
-                    name = ylabels[ind[0,0]]
-                    # use 1/dist as a confidence weight
-                    weight = 1./(max((dist[0,0] - 0.2)**2., 1e-10))
-                    print name,
-                    print weight
+                name = ylabels[ind[0,0]]
+                weight = ((1. - dist[0,0]/dist[0,1])*100)
 
-                    majpred = matchedface.updatePrediction(name, weight=weight)
-                else:
-                    print ""
+                if weight > WEIGHT_CONFIDENCE:
+                    print (name, weight) , datetime.datetime.now()
+                if weight > WEIGHT_CONFIDENCE and \
+                    (lastJSONsave is None or \
+                    time.time() - lastJSONsave > JSONsavePeriod):
+                    updateJSON(name, time.time())
+                    lastJSONsave = time.time()
+                    JSONsavePeriod = 5 + 20 * np.random.rand()  # random delay
 
-                majpred = matchedface.getMajorityPrediction()
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(cpframe, majpred, (x, y),font, 0.8, (255,255,255))
-        
-
-                # when we have enough predictions for this face...
-                if len(matchedface.lastPredictions) >= matchedface.num_majority_vote:
-                    prediction, weight = matchedface.getMajorityPredictionAndWeight()
-
-
-                    # perform VGG prediction, if this face has been seen a certain number of times
-                    # and if a certain period of time has passed since the previous VGG prediction
-                    if useVGG == 1  and (matchedface.lastVGGTime is None or time.time() - matchedface.lastVGGTime > matchedface.VGGperiod):
-                        matchedface.lastVGGTime = time.time()
-                        vggprediction = vggrecognizer.verbose_predict(roi)
-                        # verify the VGG prediction with the majority prediction, and only update
-                        # if they agree with each other
-                        if vggprediction is not None:
-                            dist, ind = prediction
-                            # print 'VGG ',
-                            # print dist,
-                            # print ind,
-                            name = ylabels_vgg[ind[0,0]]
-                            # print name,
-                            # print matchedface.VGGweight
-                            if name == prediction and weight > WEIGHT_CONFIDENCE:
-                                    updateJSON(prediction, time.time())
-
-                    else: # VGG is not being used for verification
-                        if weight > WEIGHT_CONFIDENCE and \
-                            (matchedface.lastJSONsave is None or \
-                            time.time() - matchedface.lastJSONsave > matchedface.JSONsavePeriod):
-                            updateJSON(prediction, time.time())
-                            matchedface.lastJSONsave = time.time()
-                            # print 'update successful'
+                if display_feed:
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    cv2.putText(frame, name, (x, y),font, 0.8, (255,255,255))
+            else:
+                print 'NO PREDICTION!!'
 
                 
+        if display_feed:
+            # display the FPS and seconds per frame in the top left corner
+            prevtime = currtime
+            currtime = time.time()
+            if currtime and prevtime:
+                timeperframe = currtime - prevtime
+                fps = "fps : {0}".format(1/timeperframe)
+                #tpf = "tpf : {0}".format(timeperframe)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(frame, fps, (20, 20), font, 0.5, (255,0,0))
 
-        # display the FPS and seconds per frame in the top left corner
-        prevtime = currtime
-        currtime = time.time()
-        if currtime and prevtime:
-            timeperframe = currtime - prevtime
-            fps = "fps : {0}".format(1/timeperframe)
-            #tpf = "tpf : {0}".format(timeperframe)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(cpframe, fps, (20, 20), font, 0.5, (255,0,0))
 
-
-        # Display the resulting frame
-        cv2.imshow('frame',cpframe)
-        # quit if the user presses q on the screen, 
-        if (cv2.waitKey(1) & 0xFF == ord('q')) or time.time() - timelimit_start > timelimit:
-            break
-
-        # TODO: decide what to do with this code. It used to retrain, but now it doesn't
-        # or the elapsed amount of time has passed, retrain and keep going
-        # elif time.time() - timelimit_start > timelimit:
-        #     cv2.destroyAllWindows()
-        #     for oldface in trackedfaces:
-        #         del oldface
-        #     print 'time limit reached, retrain'
-
-        #     # retrain 
-        #     if useVGG == 1:
-        #         ylabels_vgg = vggrecognizer.train(images, labels)
-        #     recognizer, ylabels = train_routine(recognizer, args['traindir'])
-        #     print 'Starting the capture.'
-            
-        #     # reset timing 
-        #     seconds_until_midnight = secondsUntilMidnight()
-        #     print 'Will reset itself in {} sec'.format(seconds_until_midnight)
-        #     timelimit_start = time.time()
+            # Display the resulting frame
+            cv2.imshow('frame',frame)
+            # quit if the user presses q on the screen, 
+            if (cv2.waitKey(1) & 0xFF == ord('q')) or time.time() - timelimit_start > timelimit:
+                break
 
     # When everything done, release the capture    
     cap.release()
