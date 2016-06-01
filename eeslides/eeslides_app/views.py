dafc28491615025dcfa2e952bec007e6439b8a9b
Sai <sai@nat-oitwireless-inside-vapornet100-a-17472.Princeton.EDU>
2016-06-01 01:25:53 -0400

    Add error handling for JSON file read/write clashes

--- eeslides/eeslides_app/views.py
+++ eeslides/eeslides_app/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import os, json, datetime, time, pytz
 from bs4 import BeautifulSoup
-import urllib2
+import urllib2, time
+
+MEMORY_DURATION = 5
+WAIT_TIME = 10
 
 # Helper method:
 # parse the html contents of http://ee.princeton.edu/slider,
@@ -79,59 +82,67 @@ def check_faces(request):
 
         readfilepath = currdir + 'static/updates_facerec.json'
         writefilepath = currdir + 'static/updates_django.json'
-        if os.path.isfile(readfilepath):
-            with open(readfilepath, 'r') as readfile:
-                filedata = json.load(readfile)
-        else:
-            filedata = {}
+        # while (time.time()%1)*100 > 40:
+        #     pass
+        try:
+            if os.path.isfile(readfilepath):
+                with open(readfilepath, 'r') as readfile:
+                    filedata = json.load(readfile)
+            else:
+                filedata = {}
 
-        timefilepath = currdir + 'static/last_updates.json'
-        if os.path.isfile(timefilepath):
-            with open(timefilepath, 'r') as readfile:
-                timedata = json.load(readfile)
-        else:
-            timedata = {}
+            timefilepath = currdir + 'static/last_updates.json'
+            if os.path.isfile(timefilepath):
+                with open(timefilepath, 'r') as readfile:
+                    timedata = json.load(readfile)
+            else:
+                timedata = {}
 
-        # convert since UTC is 4 hours ahead
-        now = datetime.datetime.now() - datetime.timedelta(hours=4)
-        currdate = now.strftime("%Y-%m-%d")
+            # convert since UTC is 4 hours ahead
+            now = datetime.datetime.now() - datetime.timedelta(hours=4)
+            currdate = now.strftime("%Y-%m-%d")
 
-        # remove anything that took place over a week ago
-        newfiledata = {}
-        for i in filedata.keys():
-            date_object = datetime.datetime.strptime(i, "%Y-%m-%d")
-            if (now - date_object).total_seconds() < 60*60*24*7:
-                newfiledata[i] = filedata[i]
+            # remove anything that took place over a week ago
+            newfiledata = {}
+            for i in filedata.keys():
+                date_object = datetime.datetime.strptime(i, "%Y-%m-%d")
+                if (now - date_object).total_seconds() < 60*60*24*7:
+                    newfiledata[i] = filedata[i]
 
-        # look at all predictions today, find the ones that haven't been updated
-        # for all in the current date
-        if currdate in newfiledata:
-            for p in newfiledata[currdate]:
-                # only look at predictions within last 5 seconds
-                if not p["parsed"] and time.time() - p["time"] < 5:
-                    p["parsed"] = True
-                    pred = p['prediction']
-                    if pred in timedata:
-                        lastpredtime = timedata[pred]
-                        # wait at least 10 seconds before adding it again
-                        if time.time() - lastpredtime > 10:
+            # look at all predictions today, find the ones that haven't been updated
+            # for all in the current date
+            if currdate in newfiledata:
+                for p in newfiledata[currdate]:
+                    # only look at predictions within last 5 seconds
+                    if not p["parsed"] and time.time() - p["time"] < MEMORY_DURATION:
+                        p["parsed"] = True
+                        pred = p['prediction']
+                        if pred in timedata:
+                            lastpredtime = timedata[pred]
+                            # wait at least 10 seconds before adding it again
+                            if time.time() - lastpredtime > WAIT_TIME:
+                                names.append(pred)
+                                timedata[pred] = time.time()
+                        else:
                             names.append(pred)
                             timedata[pred] = time.time()
-                    else:
-                        names.append(pred)
-                        timedata[pred] = time.time()
 
-        # save the updated file
-        with open(writefilepath, 'w+') as outfile:
-            json.dump(newfiledata,outfile, indent = 4, sort_keys = True)
+            # save the updated file
+            # now = time.time()
+            with open(writefilepath, 'w+') as outfile:
+                json.dump(newfiledata, outfile, indent = 4, sort_keys = True)
+            # print '\tUpdating updates_django.json took {0:.2f} seconds'.format(time.time() - now)
 
-         # save the updated file
-        with open(timefilepath, 'w+') as outfile:
-            json.dump(timedata,outfile, indent = 4, sort_keys = True)
+             # save the updated file
+            with open(timefilepath, 'w+') as outfile:
+                json.dump(timedata, outfile, indent = 4, sort_keys = True)
 
-        # take the unique names only
-        names = list(set(names))
-        print names
-        return JsonResponse({"names": names})
+            # take the unique names only
+            names = list(set(names))
+            print names
+            return JsonResponse({"names": names})
+        except:
+            print 'WARNING: Unable to read JSON file'
+            return JsonResponse({"names": []})
     else:
         return JsonResponse({"names": []})
